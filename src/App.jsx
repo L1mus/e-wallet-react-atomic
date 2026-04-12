1c2950a95a72f80f1d33503d8d14fd7106246f8f
L1mus <ali.firefox@yahoo.com>
2026-04-12 21:36:44 +0700

    feat: component dashboard layout and Route Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Landingpage from "./pages/Landingpage";
 // import { ForgotPassword } from "./pages/auth/ForgotPassword";
 // import { EnterPin } from "./pages/auth/EnterPin";
 // import { CreatePin } from "./pages/auth/CreatePin";
+import DashboardLayout from "./components/templates/DashboardLayout";
 
 function App() {
   return (
@@ -15,6 +16,13 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/enter-pin" element={<EnterPin />} />
         <Route path="/create-pin" element={<CreatePin />} /> */}
+      <Route element={<DashboardLayout />}>
+        {/* <Route path="/dashboard" element={<Placeholder title="Dashboard" />} />
+        <Route path="/transfer" element={<Placeholder title="Transfer" />} />
+        <Route path="/history" element={<Placeholder title="History" />} />
+        <Route path="/topup" element={<Placeholder title="Top Up" />} />
+        <Route path="/profile" element={<Placeholder title="Profile" />} /> */}
+      </Route>
     </Routes>
   );
 }
