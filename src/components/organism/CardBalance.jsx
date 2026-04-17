8e87de7bb621c867bb6abb2797c355f4f957630a
l1mus <alinesezcore@gmail.com>
2026-04-17 15:35:46 +0700

    feat : component CardBalance

---
+++ src/components/organism/CardBalance.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const CardBalance = ({
+  children,
+  // eslint-disable-next-line no-unused-vars
+  Icon,
+  iconStyle,
+  title,
+  balance,
+  growthIndicators,
+  growthPeriod,
+}) => {
+  return (
+    <>
+      <div className="border-2 rounded-lg border-grey-light w-full p-2 flex flex-col gap-2 items-center sm:border sm:items-start">
+        <div className="flex gap-2 items-center">
+          <div className="text-primary ">{children}</div>
+          <p className="text-xs sm:text-base">{title}</p>
+        </div>
+        <p className="text-xs sm:text-2xl font-medium text-center text ">
+          Rp.{balance}K
+        </p>
+        <div className="flex gap-0.5 items-center sm:gap-1">
+          <p className="text-xs sm:text-sm">{growthIndicators}</p>
+          <p>
+            <Icon className={iconStyle} />
+          </p>
+          <p className="text-[8px] tracking-tighter sm:text-sm">
+            {growthPeriod}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardBalance;
