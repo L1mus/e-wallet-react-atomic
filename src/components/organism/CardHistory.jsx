a9789bf14abaf695c6bb6690a2a03217d5bd7416
L1mus <ali.firefox@yahoo.com>
2026-04-19 19:16:59 +0700

    refactor: adjusment styling for responsive

--- src/components/organism/CardHistory.jsx
+++ src/components/organism/CardHistory.jsx
@@ -1,20 +1,22 @@
-import React from "react";
 import Avatar from "../atoms/Avatar";
 
 const CardHistory = ({ name, status, imageSrc, amount }) => {
   return (
-    <>
-      <div className="grid grid-cols-2 items-center sm:grid-cols-3 p-1">
-        <div className="flex gap-3 items-center sm:col-span-2 sm:justify-evenly">
-          <Avatar imageSrc={imageSrc} className="rounded-lg" />
-          <div>
-            <p className="font-semibold text-sm">{name}</p>
-            <p className="text-xs">{status}</p>
-          </div>
+    <div className="flex items-center justify-between py-2 gap-2">
+      <div className="flex items-center gap-3 overflow-hidden">
+        <Avatar
+          imageSrc={imageSrc}
+          className="rounded-lg w-10 h-10 sm:w-11 sm:h-11 shadow-sm shrink-0"
+        />
+
+        <div className="flex flex-col overflow-hidden">
+          <p className="font-semibold text-sm text-black truncate">{name}</p>
+          <p className="text-xs text-grey mt-0.5 truncate">{status}</p>
         </div>
-        <p className="text-end font-semibold">{amount}</p>
       </div>
-    </>
+
+      <p className="font-semibold text-sm text-right shrink-0">{amount}</p>
+    </div>
   );
 };
 
