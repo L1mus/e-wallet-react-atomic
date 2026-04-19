4c6facc69e9a66c8c441697ff72b3259a6a37fcd
L1mus <ali.firefox@yahoo.com>
2026-04-19 19:21:45 +0700

    refactor: add components Headers and Sidebar

--- src/components/templates/DashboardLayout.jsx
+++ src/components/templates/DashboardLayout.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
 import Sidebar from "../organism/Sidebar";
+import Header from "../organism/Header";
 
 const DashboardLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
-    <div className="flex bg-grey-light/30">
-      <Sidebar />
-      <main className="flex-1 p-6 md:p-8 overflow-y-auto">
-        <Outlet />
-      </main>
+    <div className="flex flex-col min-h-screen bg-grey-light/30">
+      <Header
+        isLogin={true}
+        isDashboard={true}
+        onOpenSidebar={() => setIsSidebarOpen(true)}
+      />
+
+      <div className="flex flex-1 relative">
+        <Sidebar
+          isOpen={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+        />
+
+        <main className="flex-1 p-6 md:p-7 bg-white">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
