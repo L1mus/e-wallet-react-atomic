1e3963647b0499b4053e9500850441dccb2ec8c6
L1mus <ali.firefox@yahoo.com>
2026-04-19 16:29:01 +0700

    reafactor: add new Route Dashboards

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
-import { Routes, Route } from "react-router";
-import Landingpage from "./pages/Landingpage";
+import { Routes, Route, Navigate } from "react-router";
+
+import LandingPage from "./pages/LandingPage";
 // import { Login } from "./pages/auth/Login";
 // import { Register } from "./pages/auth/Register";
 // import { ForgotPassword } from "./pages/auth/ForgotPassword";
 // import { EnterPin } from "./pages/auth/EnterPin";
 // import { CreatePin } from "./pages/auth/CreatePin";
+
 import DashboardLayout from "./components/templates/DashboardLayout";
+import Dashboard from "./pages/dashboard/Dashboard";
+import History from "./pages/dashboard/History";
+import Transfer from "./pages/dashboard/Transfer";
+import TransferDetail from "./pages/dashboard/TransferDeatail";
+import TopUp from "./pages/dashboard/TopUp";
+import Profile from "./pages/dashboard/Profile";
+import ChangePassword from "./pages/dashboard/ChangePassword";
+import ChangePin from "./pages/dashboard/ChangePin";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landingpage />} />
+      <Route path="/" element={<LandingPage />} />
       {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/enter-pin" element={<EnterPin />} />
         <Route path="/create-pin" element={<CreatePin />} /> */}
-      <Route element={<DashboardLayout />}>
-        {/* <Route path="/dashboard" element={<Placeholder title="Dashboard" />} />
-        <Route path="/transfer" element={<Placeholder title="Transfer" />} />
-        <Route path="/history" element={<Placeholder title="History" />} />
-        <Route path="/topup" element={<Placeholder title="Top Up" />} />
-        <Route path="/profile" element={<Placeholder title="Profile" />} /> */}
+
+      <Route path="/dashboard" element={<DashboardLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="history" element={<History />} />
+        <Route path="transfer" element={<Transfer />} />
+        <Route path="transfer/:id" element={<TransferDetail />} />
+        <Route path="topup" element={<TopUp />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="profile/change-password" element={<ChangePassword />} />
+        <Route path="profile/change-pin" element={<ChangePin />} />
       </Route>
+
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
