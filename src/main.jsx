572ff33afdabeddd2fa9174c0b023af9cb3f5ed1
l1mus <alinesezcore@gmail.com>
2026-04-21 06:55:28 +0700

    import redux sotre ant redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 import "./globals.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store.js";
+import { ToastContainer } from "react-toastify";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <ToastContainer />
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
 );
