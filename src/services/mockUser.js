267affe9912ae9cd54f414cdc1c2d19d44e750cc
L1mus <ali.firefox@yahoo.com>
2026-04-27 06:47:41 +0700

    feat: data mock for user

---
+++ src/services/mockUser.js
@@ -0,0 +1,33 @@
+export const mockUsers = [
+  {
+    id: "1",
+    username: "Ghaluh Wizard",
+    email: "ghaluhwizz@gmail.com",
+    phone: "6282116304337",
+    pin: "123456",
+    balance: 120000,
+    isVerified: true,
+    profilePicture:
+      "https://ui-avatars.com/api/?name=Ghaluh+Wizard&background=random",
+  },
+  {
+    id: "2",
+    username: "Robert Fox",
+    email: "robert.fox@example.com",
+    phone: "2395550108",
+    pin: "111111",
+    balance: 500000,
+    isVerified: true,
+    profilePicture: "https://i.pravatar.cc/150?u=u2",
+  },
+  {
+    id: "3",
+    username: "Ali Mustadji",
+    email: "limustadji@example.com",
+    phone: "6285156534946",
+    pin: "777777",
+    balance: 1000000,
+    isVerified: true,
+    profilePicture: "https://i.pravatar.cc/150?u=u3",
+  },
+];
