57e35c1ce9aef72372e89618a0f2743be49d258d
L1mus <ali.firefox@yahoo.com>
2026-05-01 09:12:31 +0700

    refactor: condition if user login and dont have pin

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -2,9 +2,8 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router";
 
 const PublicRoute = () => {
-  const { isLogin } = useSelector((state) => state.loginReducer);
-
-  if (isLogin) {
+  const { isLogin, loginUser } = useSelector((state) => state.loginReducer);
+  if (isLogin && loginUser.pin !== "") {
     return <Navigate to="/dashboard" replace />;
   }
 
